80a70a46c22dc33e2557b65400ac1f2eabc71ccb
rae <lukehammer104@gmail.com>
2022-06-29 21:35:41 -0700

    added echo igniter - texture temporary, to be changed

--- src/main/java/net/rae/tutorialmod/TutorialMod.java
+++ src/main/java/net/rae/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package net.rae.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.rae.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,5 +12,8 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+
+		ModItems.registerModItems();
+
 	}
 }
---
+++ src/main/java/net/rae/tutorialmod/item/ModItems.java
@@ -0,0 +1,21 @@
+package net.rae.tutorialmod.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.rae.tutorialmod.TutorialMod;
+
+public class ModItems {
+
+    public static final Item ECHO_IGNITER = registerItem("echo_igniter",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(TutorialMod.MOD_ID, name), item);
+    }
+    public static void registerModItems() {
+        TutorialMod.LOGGER.debug("Registering Mod Items for " + TutorialMod.MOD_ID);
+    }
+}
