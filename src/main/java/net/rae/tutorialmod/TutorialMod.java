f8a586701806d2cd6e86e08e967dd1d40c061ceb
rae <lukehammer104@gmail.com>
2022-06-29 20:45:29 -0700

    added important comment

--- src/main/java/net/rae/tutorialmod/TutorialMod.java
+++ src/main/java/net/rae/tutorialmod/TutorialMod.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// very important comment
 public class TutorialMod implements ModInitializer {
 	public static final String MOD_ID = "tutorialmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
