79ea18f1d040fed3f873382a1c32db2295e483fc
rae <lukehammer104@gmail.com>
2022-07-03 16:08:56 -0700

    registered new sculked and ash wood types

--- src/main/java/net/rae/darkdeep/block/ModBlocks.java
+++ src/main/java/net/rae/darkdeep/block/ModBlocks.java
@@ -3,7 +3,9 @@ package net.rae.darkdeep.block;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.Material;
+import net.minecraft.block.PillarBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -15,6 +17,28 @@ import net.rae.darkdeep.item.ModItemGroup;
 public class ModBlocks {
     public static final Block COMPRESSED_DEEPSLATE = registerBlock("compressed_deepslate",
             new Block(FabricBlockSettings.of(Material.STONE).strength(8f).requiresTool()), ModItemGroup.THE_CAVERN);
+
+    public static final Block SCULKED_LOG = registerBlock("sculked_log",
+            new PillarBlock(FabricBlockSettings.copy(Blocks.OAK_LOG).strength(4.5f).requiresTool()), ModItemGroup.THE_CAVERN);
+    public static final Block SCULKED_WOOD = registerBlock("sculked_wood",
+            new PillarBlock(FabricBlockSettings.copy(Blocks.OAK_WOOD).strength(4.5f).requiresTool()), ModItemGroup.THE_CAVERN);
+    public static final Block STRIPPED_SCULKED_LOG = registerBlock("stripped_sculked_log",
+            new PillarBlock(FabricBlockSettings.copy(Blocks.STRIPPED_OAK_LOG).strength(4.5f).requiresTool()), ModItemGroup.THE_CAVERN);
+    public static final Block STRIPPED_SCULKED_WOOD = registerBlock("stripped_sculked_log",
+            new PillarBlock(FabricBlockSettings.copy(Blocks.STRIPPED_OAK_WOOD).strength(4.5f).requiresTool()), ModItemGroup.THE_CAVERN);
+    public static final Block ASH_LOG = registerBlock("ash_log",
+            new PillarBlock(FabricBlockSettings.copy(Blocks.OAK_LOG).strength(4f).requiresTool()), ModItemGroup.THE_CAVERN);
+    public static final Block ASH_WOOD = registerBlock("ash_wood",
+            new PillarBlock(FabricBlockSettings.copy(Blocks.OAK_WOOD).strength(4f).requiresTool()), ModItemGroup.THE_CAVERN);
+    public static final Block STRIPPED_ASH_LOG = registerBlock("stripped_ash_log",
+            new PillarBlock(FabricBlockSettings.copy(Blocks.STRIPPED_OAK_LOG).strength(4f).requiresTool()), ModItemGroup.THE_CAVERN);
+    public static final Block STRIPPED_ASH_WOOD = registerBlock("stripped_ash_wood",
+            new PillarBlock(FabricBlockSettings.copy(Blocks.STRIPPED_OAK_WOOD).strength(4f).requiresTool()), ModItemGroup.THE_CAVERN);
+
+    public static final Block SCULKED_PLANKS = registerBlock("sculked_planks",
+            new Block(FabricBlockSettings.copy(Blocks.OAK_PLANKS).strength(4f).requiresTool()), ModItemGroup.THE_CAVERN);
+    public static final Block ASH_PLANKS = registerBlock("ash_planks",
+            new Block(FabricBlockSettings.copy(Blocks.OAK_PLANKS).strength(4f).requiresTool()), ModItemGroup.THE_CAVERN);
 
 
     private static Block registerBlock(String name, Block block, ItemGroup tab) {
